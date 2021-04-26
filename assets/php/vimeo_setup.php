bc755be3ee4d6c405ed872c0afd7826415e9fc9f
Loïc <59501884+LoicE5@users.noreply.github.com>
2021-04-26 23:57:36 +0200

    replaced

---
+++ assets/php/vimeo_setup.php
@@ -0,0 +1,21 @@
+<?php
+require '../vendor/autoload.php';
+require_once('../getid3/getid3.php');
+
+use Vimeo\Vimeo;
+
+$client_id = "credential";
+$client_secret = "credential";
+$access_token = "credential";
+// $old_access_token = "credential";
+
+$vimeo = new Vimeo($client_id, $client_secret, $access_token);
+
+$response = $vimeo->request('/tutorial', array(), 'GET');
+
+// Récupérer la durée
+$getID3 = new getID3;
+
+$filename = $_FILES["video"]['tmp_name'];
+$file = $getID3->analyze($filename);
+?>
